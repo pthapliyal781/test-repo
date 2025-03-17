34f8436d166e51bc7593543ff1c177988b53e8c4
Praveen Chandra Thapliyal <pthapliyal781@gmail.com>
2025-03-17 13:06:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, PCT!');
 });
 
 const port = process.env.PORT || 3000;
